72b569dc9ad46715cf5f0188ee1e214fc14bc6fb
Kirill <kirill.dudarev.20020523@gmail.com>
2023-01-28 23:23:46 +1000

    Two Sum

---
+++ Algorithms/1. Two Sum/Two Sum (Two-pass Hash Table).py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]] 
